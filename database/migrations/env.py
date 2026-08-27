68db915598bb1e58771b46805e83dc3a89f3648f
abdulhameedmahmood1-spec <abdulhameedmahmood1@gmail.com>
2026-08-27 11:04:06 +0100

    Update env.py

--- database/migrations/env.py
+++ database/migrations/env.py
@@ -2,11 +2,13 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
 from alembic import context
+from backend.app.core.config import settings
 
 from backend.app.db.database import Base
 from backend.app.models import Vehicle, GateCheck
 
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
